40f2a8b2c72ab73847ba49f01cb6e9be183626a2
jafzak <zakariyajaafar53@gmail.com>
2024-01-17 10:33:33 +0100

    Corrected the h1 tag, it wasn't closed properly

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var button = document.getElementById("sub");
 
 
 button.addEventListener('click', () => {
-  var dolAmount = Amount.value * 415.19;
+  var dolAmount = Amount.value * 895.50;
   document.getElementById("beg").innerText = Amount.value + " " + "united states dollar is";
   document.getElementById("end").innerText = dolAmount + " Nigerian Naira";
   
